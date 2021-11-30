2e4b2291e53d8794044f9a7b1f10978830531567
vipulkumar4321 <47936732+vipulkumar4321@users.noreply.github.com>
2021-11-30 12:53:23 +0530

    Update InputImage.js

--- src/components/InputImage/InputImage.js
+++ src/components/InputImage/InputImage.js
@@ -30,7 +30,7 @@ class InputImage extends React.Component {
     const fd = new FormData();
     fd.append("file", this.state.selectedFile, this.state.selectedFile.name);
     axios 
-      .post("https://15.207.247.37/predict", {mode:'cors'}, fd, {
+      .post("https://15.207.247.37/predict", fd, {
         onUploadProgress: (ProgressEvent) => {
           console.log(
             "Upload Progress: " +
